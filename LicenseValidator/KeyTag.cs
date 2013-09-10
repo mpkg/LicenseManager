6b62809275ab287940cdafb674e3984d1c33f2a8
mpkg <mayank.gadodia87@gmail.com>
2013-09-10 16:50:04 +0530

    commit

--- LicenseValidator/KeyTag.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace IOTAP.LicenseManagement
-{
-    public class KeyTag
-    {
-        public static readonly string RSAKeyValue = "RSAKeyValue";
-        public static readonly string Exponent = "Exponent";
-        public static readonly string Modulus = "Modulus";
-    }
-}
